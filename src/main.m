02716794c8b60f26cfd83e9fcce2fb9a6d0423db
Luciano Oliveira <luciano.laoz@gmail.com>
2021-04-24 06:31:28 -0700

    user input for cuts

--- src/main.m
+++ src/main.m
@@ -1,10 +1,10 @@
-% clc
-% clear variables
-% close all
-% 
+clc
+clear variables
+close all
+
 % %%%%%%%%%% PARAMETERS %%%%%%%%%%
  
-% filename = "hamster";
+filename = "hamster";
 half_window = 4;
 n_skip = 7; % skip n frames ahead and before (even number for this implementation)
 min_seq = 7; % minimum number of frames in a sequence (without cuts)
@@ -12,6 +12,8 @@ threshold = 2;
 n_jump_options = 1; % find cuts with n number of options to jump (n = 1 for best results)
 use_cross_fading = true;
 should_finish_at_last_frame = false;
+user_input = true;
+max_cuts = 5;
 
 % filename = "clock";
 % half_window = 4;
@@ -21,15 +23,17 @@ should_finish_at_last_frame = false;
 % n_jump_options = 1;
 % use_cross_fading = true;
 % should_finish_at_last_frame = false;
+% user_input = false;
+% max_cuts = 10;
 
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% 
-% input_folder = "../input/";
-% output_folder = "../output/";
+
+input_folder = "../input/";
+output_folder = "../output/";
 jumps_folder = "../jumps/";
-% 
-% % Load video
-% [video, h, w, n_frames] = loadVideo(input_folder+filename + ".mp4");
+
+% Load video
+[video, h, w, n_frames] = loadVideo(input_folder+filename + ".mp4");
 
 if min_seq <= half_window
     min_seq = half_window+1;
@@ -51,19 +55,23 @@ TF = islocalmax(M);
 M = M .* TF;
 jumpable_frames = sum(M>0);
 
-at_least_one_option = sum(M>=1);
-at_least_two_options = sum(M>=2);
-at_least_three_options = sum(M>=3);
+% at_least_one_option = sum(M>=1);
+% at_least_two_options = sum(M>=2);
+% at_least_three_options = sum(M>=3);
+
+if jumpable_frames == 0
+    fprintf('No cuts found');
+    return; % exit program
+end
 
 list_frames = 1:min_seq;
 
 % Find cuts
 count = 0;
-total_cuts = 5;
-for i = 1:total_cuts
+for i = 1:max_cuts
     [rows, cols, n_cuts_response] = findCuts(list_frames, D, M, n_frames, n_jump_options);
     if numel(rows) > 0
-        fprintf('Found cut %d/%d\n', i, total_cuts);
+        fprintf('Found cut %d/%d\n', i, max_cuts);
     end
     copy_rows = rows;
     copy_cols = cols;
@@ -101,31 +109,51 @@ for i = 1:total_cuts
     %rows(1) = 26; % 28 29 8 18
     %cols(1) = 6; % 7 8 3 13
     n_cuts_options_response = numel(rows);
-    % if n_cuts_response_new > 0
+    if n_cuts_options_response == 0
+        fprintf('No more cuts available');
+        break; % gets out of loop to start rendering
+    end
+    
     if n_cuts_options_response >= n_jump_options
         count = count + 1;
         [next_jumpable_frame, next_idx] = min(rows(:,find((all(rows>list_frames(end))),1)));
         
-        % Render current list of frames
-        render(list_frames:next_jumpable_frame, video, jumps_folder+"0_current", half_window, use_cross_fading, false);
-        
-        for k = 1:n_cuts_options_response
-            % Render before option
-            render(list_frames:rows(k), video, jumps_folder+string(k)+"_before", half_window, use_cross_fading, false);
+        if user_input
+            % Render current list of frames
+            current_frame_end = next_jumpable_frame + min_seq;
+            if current_frame_end > n_frames
+                current_frame_end = n_frames;
+            end
+            render([list_frames list_frames(end)+1:current_frame_end], video, jumps_folder+"0_current", half_window, use_cross_fading, false);
+
+            for k = 1:n_cuts_options_response
+                % Render before option
+                render([list_frames list_frames(end)+1:rows(k)], video, jumps_folder+string(k)+"_before", half_window, use_cross_fading, false);
+
+                % Render option
+                render(cols(k):cols(k)+min_seq, video, jumps_folder+string(k)+"_option", half_window, use_cross_fading, false);
+            end
             
-            % Render option
-            render(cols(k):cols(k)+min_seq-1, video, jumps_folder+string(k)+"_option", half_window, use_cross_fading, false);
+            % Ask user for the chosen option
+            op = -1;
+            while op < 0 || op > n_cuts_options_response
+                op = input("Choose option inside 'jump' folder (0: no cuts; 1-"+string(n_cuts_options_response)+" for cuts): ");
+            end
+            
+            % Synthesize option
+            list_frames = synthesis(list_frames, n_frames, rows(op), cols(op), min_seq);
+        else
+            % Randomize options
+            id = randi(n_cuts_options_response);
+            list_frames = synthesis(list_frames, n_frames, rows(id), cols(id), min_seq);
+            %list_frames = synthesis(list_frames, n_frames, rows(2), cols(2), min_seq);
         end
         
-        id = randi(n_cuts_options_response);
-        list_frames = synthesis(list_frames, n_frames, rows(id), cols(id), min_seq);
-        
-        %list_frames = synthesis(list_frames, n_frames, rows(2), cols(2), min_seq);
     end
 end
 
 if should_finish_at_last_frame
-    list_frames = [list_frames list_frames(end):n_frames];
+    list_frames = [list_frames list_frames(end)+1:n_frames];
 end
 
 if count > 0
